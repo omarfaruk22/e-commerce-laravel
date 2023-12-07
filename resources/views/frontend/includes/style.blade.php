45f0f3ee8c04e56c04ffd897711edbd20803d3d5
omar <2070omarfaruk@gmail.com>
2023-12-07 23:59:40 +0600

    3rd

---
+++ resources/views/frontend/includes/style.blade.php
@@ -0,0 +1,5 @@
+   <!-- Favicon -->
+   <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/theme/favicon.svg">
+   <!-- Template CSS -->
+   <link rel="stylesheet" href="assets/css/plugins/animate.min.css">
+   <link rel="stylesheet" href="assets/css/main.css?v=5.2">
