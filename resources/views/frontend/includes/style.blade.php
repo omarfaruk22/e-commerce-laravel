66200f75ab8caf5c7f061db5a5148a3301ae073a
omar <2070omarfaruk@gmail.com>
2023-12-10 01:02:18 +0600

    mastering

--- resources/views/frontend/includes/style.blade.php
+++ resources/views/frontend/includes/style.blade.php
@@ -1,5 +1,5 @@
    <!-- Favicon -->
-   <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/theme/favicon.svg">
+   <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/imgs/theme/favicon.svg')}}">
    <!-- Template CSS -->
-   <link rel="stylesheet" href="assets/css/plugins/animate.min.css">
-   <link rel="stylesheet" href="assets/css/main.css?v=5.2">
+   <link rel="stylesheet" href="{{asset('frontend/css/plugins/animate.min.css')}}">
+   <link rel="stylesheet" href="{{asset('frontend/css/main.css?v=5.2')}}">
